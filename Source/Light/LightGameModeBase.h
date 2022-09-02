b1710239241abb3f424a3d42a1ba5e5d50868941
JinBingyang <3170100821@zju.edu.cn>
2022-09-02 12:50:11 +0800

    wrong

---
+++ Source/Light/LightGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHT_API ALightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
